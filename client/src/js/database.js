21ad318940de4c20b877a192e9b0e04851fc188d
PFZM <pfzm@hotmail.com>
2022-01-23 21:44:46 +1100

    fix bug on editor.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
-  const jateDb = await openDB("jateDb", 1);
+  const jateDb = await openDB("jate", 1);
   const store = jateDb.transaction("jate", "readwrite").objectStore("jate");
   const request = store.put({ id: 1, value: content });
   const result = await request;
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
 // Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
-  const jateDb = await openDB("jateDb", 1);
+  const jateDb = await openDB("jate", 1);
   const store = jateDb.transaction("jate", "readonly").objectStore("jate");
   const request = store.getAll();
   const result = await request;
